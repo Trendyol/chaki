cf1b3e61bd39708470d029ae81515a55ee5be0df
evren.ispiroglu <evren.ispiroglu@trendyol.com>
2024-06-27 16:04:38 +0300

    feat(server): predefined handler functions

---
+++ modules/server/handler/handlers.go
@@ -0,0 +1,23 @@
+package handler
+
+import "net/http"
+
+func Get[Req, Res any](path string, h HandlerFunc[Req, Res]) Handler {
+	return New[Req, Res](http.MethodGet, path, h)
+}
+
+func Post[Req, Res any](path string, h HandlerFunc[Req, Res]) Handler {
+	return New[Req, Res](http.MethodGet, path, h)
+}
+
+func Patch[Req, Res any](path string, h HandlerFunc[Req, Res]) Handler {
+	return New[Req, Res](http.MethodPatch, path, h)
+}
+
+func Put[Req, Res any](path string, h HandlerFunc[Req, Res]) Handler {
+	return New[Req, Res](http.MethodPut, path, h)
+}
+
+func Delete[Req, Res any](path string, h HandlerFunc[Req, Res]) Handler {
+	return New[Req, Res](http.MethodDelete, path, h)
+}
