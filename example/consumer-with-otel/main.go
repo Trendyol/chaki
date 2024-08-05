93986ff8cc9753c08f24e97bc9ebcc88682533ad
Ahmetcan ÖZCAN <ahmetcanozcan7@gmail.com>
2024-08-05 11:39:26 +0300

    Add example

---
+++ example/consumer-with-otel/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"github.com/Trendyol/chaki"
+	"github.com/Trendyol/chaki/logger"
+	"github.com/Trendyol/chaki/modules/kafka"
+	"github.com/Trendyol/chaki/modules/kafka/consumer"
+	"github.com/Trendyol/chaki/modules/otel"
+	otelkafka "github.com/Trendyol/chaki/modules/otel/kafka"
+)
+
+func main() {
+	app := chaki.New()
+
+	app.WithOption(
+		chaki.WithConfigPath("config.yaml"),
+	)
+
+	app.Use(
+		otel.Module(),
+		otelkafka.Module(),
+		kafka.Module(),
+	)
+
+	app.Provide(
+		newConsumer,
+	)
+
+	logger.Fatal(app.Start())
+
+}
+
+func newConsumer() consumer.Consumer {
+	return consumer.NewFn("example", func(msg *consumer.Message) error {
+		// otel span defined in context
+		// logger can log spanId and traceId
+		logger.From(msg.Context).Info("message here")
+		return nil
+	})
+}
