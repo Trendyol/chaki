08684c2533228b0ea7ed8a8f8858a84498262d76
ispiroglu <evrenn.ispiroglu@gmail.com>
2024-08-05 22:00:38 +0300

    feat(swagger): initial swagger implementation

---
+++ modules/swagger/defs.go
@@ -0,0 +1,83 @@
+package swagger
+
+import (
+	"reflect"
+	"strings"
+
+	"github.com/Trendyol/chaki/internal/typlect"
+)
+
+func buildDefinitions(eds []EndpointDef) m {
+	defs := make(m)
+	for _, ed := range eds {
+		buildModelDefinition(defs, ed.RequestType, true)
+		buildModelDefinition(defs, ed.ResponseType, false)
+	}
+
+	return defs
+
+}
+
+func buildModelDefinition(defs m, t reflect.Type, isReq bool) {
+	if t == typlect.TypeNoParam {
+		return
+	}
+
+	if t.Kind() == reflect.Slice {
+		t = t.Elem()
+	}
+
+	if t.Kind() == reflect.Pointer {
+		t = t.Elem()
+	}
+
+	if t.Kind() != reflect.Struct {
+		return
+	}
+
+	var smr []string
+	smp := m{}
+	for i := 0; i < t.NumField(); i++ {
+		var (
+			f  = t.Field(i)
+			ft = f.Type
+		)
+
+		//TODO: pick required fields to `smr` list
+
+		// build sub type definitions
+		if ft != typlect.TypeTime && ft.Kind() == reflect.Struct {
+			buildModelDefinition(defs, ft, isReq)
+		}
+
+		if ft.Kind() == reflect.Slice && ft.Elem().Kind() == reflect.Struct {
+			buildModelDefinition(defs, ft.Elem(), isReq)
+		}
+
+		if !isReq || f.Tag.Get("json") != "" {
+			smp[getFieldName(f)] = getPropertyField(f.Type)
+		}
+	}
+
+	if len(smp) > 0 {
+		mi := m{
+			"type":       "object",
+			"properties": smp,
+		}
+
+		if len(smr) > 0 {
+			mi["required"] = smr
+		}
+
+		defs[getNameFromType(t)] = mi
+	}
+
+}
+
+func getFieldName(f reflect.StructField) string {
+	if tag := f.Tag.Get("json"); tag != "" {
+		return strings.Split(tag, ",")[0] // ignore ',omitempty'
+	}
+
+	return f.Name
+}
---
+++ modules/swagger/paths.go
@@ -0,0 +1,113 @@
+package swagger
+
+import (
+	"reflect"
+	"strings"
+)
+
+func buildPaths(eds []EndpointDef) m {
+	p := make(m)
+	for _, ed := range eds {
+		if strings.HasPrefix(ed.Endpoint, "/__") {
+			continue
+		}
+
+		desc := m{
+			"tags":        []string{ed.Group},
+			"summary":     ed.Name,
+			"description": "",
+			"consumes": []string{
+				"application/json",
+			},
+			"produces": []string{
+				"application/json",
+			},
+			"externalDocs": m{},
+		}
+
+		if pr := getParamters(ed.RequestType); pr != nil {
+			desc["parameters"] = pr
+		}
+
+		desc["responses"] = m{
+			"200": m{
+				"description": "successful operation",
+				"schema":      getPropertyField(ed.ResponseType),
+			},
+		}
+
+		meth := strings.ToLower(ed.Method)
+		swagp := toSwaggerPath(ed.Endpoint)
+
+		existdesc, ok := p[swagp].(m)
+		if !ok {
+			existdesc = m{}
+		}
+
+		existdesc[meth] = desc
+		p[swagp] = existdesc
+
+	}
+
+	return p
+}
+
+func getParamters(t reflect.Type) []m {
+	if t.Kind() == reflect.Pointer {
+		t = t.Elem()
+	}
+
+	if t.Kind() == reflect.Slice {
+		mi := arrayProperty(t)
+		mi["name"] = "body"
+		mi["in"] = "body"
+		return []m{mi}
+	}
+
+	var p []m
+	var hasBody bool
+	for i := 0; i < t.NumField(); i++ {
+		f := t.Field(i)
+
+		required := f.Tag.Get("required") == "true"
+
+		if n := f.Tag.Get("param"); n != "" {
+			pi := getPropertyField(f.Type)
+			pi["in"] = "path"
+			pi["name"] = n
+			pi["description"] = ""
+			pi["required"] = true
+			p = append(p, pi)
+		}
+
+		if n := f.Tag.Get("query"); n != "" {
+			pi := getPropertyField(f.Type)
+			pi["in"] = "query"
+			pi["name"] = n
+			pi["description"] = ""
+			if required {
+				pi["required"] = true
+			}
+			p = append(p, pi)
+		}
+
+		if f.Tag.Get("json") != "" {
+			hasBody = true
+		}
+
+	}
+
+	if hasBody {
+		p = append(p, m{
+			"in":          "body",
+			"name":        "body",
+			"description": "",
+			"required":    true,
+			"schema": m{
+				refKey: withDefinitionPrefix(getNameFromType(t)),
+			},
+		})
+	}
+
+	return p
+}
