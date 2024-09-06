115da23d1fa17f247a524f279c0f9cfd9f0e1a41
Ahmetcan ÖZCAN <ahmetcanozcan7@gmail.com>
2024-09-06 13:43:31 +0300

    fix(client-wrapper): Update ctx binder nil-safe

--- modules/client/wrapper.go
+++ modules/client/wrapper.go
@@ -19,16 +19,24 @@ func WithTransport(tr http.RoundTripper) DriverWrapper {
 func withCtxBinder() DriverWrapper {
 	return func(restyClient *resty.Client) *resty.Client {
 		return restyClient.OnBeforeRequest(func(c *resty.Client, r *resty.Request) error {
-			context := r.Context()
+			ctx := r.Context()
+			h := map[string]string{}
+			if v := ctxvaluer.CorrelationID.Get(ctx, ""); v != "" {
+				h[ctxvaluer.CorrelationIDKey] = v
+			}
 
-			h := map[string]string{
-				ctxvaluer.CorrelationIDKey: context.Value(ctxvaluer.CorrelationIDKey).(string),
-				ctxvaluer.ExecutorUserKey:  context.Value(ctxvaluer.ExecutorUserKey).(string),
-				ctxvaluer.AgentNameKey:     context.Value(ctxvaluer.AgentNameKey).(string),
-				ctxvaluer.OwnerKey:         context.Value(ctxvaluer.OwnerKey).(string),
+			if v := ctxvaluer.ExecutorUser.Get(ctx, ""); v != "" {
+				h[ctxvaluer.ExecutorUserKey] = v
+			}
+
+			if v := ctxvaluer.AgentName.Get(ctx, ""); v != "" {
+				h[ctxvaluer.AgentNameKey] = v
+			}
+
+			if v := ctxvaluer.Owner.Get(ctx, ""); v != "" {
+				h[ctxvaluer.OwnerKey] = v
 			}
 			r.SetHeaders(h)
-
 			return nil
 		})
 	}
